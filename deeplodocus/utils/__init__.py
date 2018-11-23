df14a9f28db3cac8b1df327cd2b427268cd82f77
Unknown <alix.m.leroy@cranfield.ac.uk>
2018-11-23 17:18:02 +0000

    Comment get_main_path() method

--- deeplodocus/utils/__init__.py
+++ deeplodocus/utils/__init__.py
@@ -1,10 +1,27 @@
 import __main__
-
 import os
 
 
 def get_main_path():
     """
-    :return:
+    AUTHORS:
+    --------
+
+    :author: Alix Leroy
+
+    DESCRIPTION:
+    ------------
+
+    Get the path to the main running file.
+
+    PARAMETERS:
+    -----------
+
+    None
+
+    RETURN:
+    -------
+
+    :return: The path to the main file
     """
     return os.path.dirname(os.path.abspath(__main__.__file__))
