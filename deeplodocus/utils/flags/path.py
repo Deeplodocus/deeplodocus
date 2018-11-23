2ef07381936a1df0cf11e1c6e17f6a10da4eeaa6
Unknown <alix.m.leroy@cranfield.ac.uk>
2018-11-23 12:22:19 +0000

    Fix issue relative path

--- deeplodocus/utils/flags/path.py
+++ deeplodocus/utils/flags/path.py
@@ -8,6 +8,6 @@ DEEP_PATH_HISTORY = r"%s/results/history" % get_main_path()
 DEEP_PATH_SAVE_MODEL = r"%s/results/models" % get_main_path()
 
 
-DEEP_PATH_MODULES = ".modules"                                      # Relative path to the "modules" module
+DEEP_PATH_MODULES = "modules"                                      # Relative path to the "modules" module
 DEEP_PATH_MODELS = "%s.models" % DEEP_PATH_MODULES
 DEEP_PATH_OPTIMIZERS = "%s.optimizers" % DEEP_PATH_MODULES
