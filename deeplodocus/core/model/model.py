0980f0f63e85fa147db7933239ac487761e77d9c
Unknown <alix.m.leroy@cranfield.ac.uk>
2018-11-23 17:16:40 +0000

    Update Model class
    Add comments
    Make load function static
    Remove config attribute (no need to be kept in memory)
    Add typing

--- deeplodocus/core/model/model.py
+++ deeplodocus/core/model/model.py
@@ -1,26 +1,74 @@
 
 from deeplodocus.utils.dict_utils import check_kwargs
 from deeplodocus.utils.generic_utils import get_module
-
+from deeplodocus.utils.namespace import Namespace
 
 class Model(object):
+    """
+    AUTHORS:
+    --------
 
-    def __init__(self, config):
-        self.model = None
-        self.config = config
-        self.load_model()
+    :author: Samuel Westlake
+    :author: Alix Leroy
 
-    def load_model(self):
+    DESCRIPTION:
+    ------------
+
+    Model class containing the model
+    """
+
+    def __init__(self, config: Namespace):
+        self.model = self.load_model(config)
+
+    @staticmethod
+    def load(config: Namespace):
         """
-        :return:
+        AUTHORS:
+        --------
+
+        :author: Samuel Westlake
+        :author: Alix Leroy
+
+        DESCRIPTION:
+        ------------
+
+        Load the model
+
+        PARAMETERS:
+        -----------
+
+        :param config(Namespace): The parameters from the model config file
+
+        RETURN:
+        -------
+
+        :return: None
         """
-        model = get_module(module=self.config.module,
-                           name=self.config.name)
-        kwargs = check_kwargs(self.config.kwargs)
-        self.model = model(**kwargs)
+        model = get_module(module=config.module,
+                           name=config.name)
+        kwargs = check_kwargs(config.kwargs)
+        return model(**kwargs)
 
     def get(self):
         """
-        :return:
+        AUTHORS:
+        --------
+
+        :author: Samuel Westlake
+
+        DESCRIPTION:
+        ------------
+
+        Get the model
+
+        PARAMETERS:
+        -----------
+
+        None
+
+        RETURN:
+        -------
+
+        :return: None
         """
         return self.model
