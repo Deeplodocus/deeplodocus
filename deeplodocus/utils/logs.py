6e137d330366573316f61abd41613b5046aff51c
samuelwestlake <samuelwestlake@hotmial.co.uk>
2018-12-19 20:52:43 +0000

    Tidy utils.logs.py

--- deeplodocus/utils/logs.py
+++ deeplodocus/utils/logs.py
@@ -13,7 +13,7 @@ class Logs(object):
     AUTHORS:
     --------
 
-    :author: Alix Leroy
+    :author: Alix Leroy and Samuel Westlake
 
     DESCRIPTION:
     ------------
@@ -21,7 +21,7 @@ class Logs(object):
     A class which manages the logs
     """
 
-    def __init__(self, d_type: str,
+    def __init__(self, log_type: str,
                  directory: str = "%s/logs" % os.path.dirname(os.path.abspath(__main__.__file__)),
                  extension: str = DEEP_EXT_CSV,
                  write_time=True) -> None:
@@ -39,7 +39,7 @@ class Logs(object):
         PARAMETERS:
         -----------
 
-        :param d_type: str: The log type (notification, history
+        :param log_type: str: The log type (notification, history
         :param directory: str
         :param extension: str:
         :param write_time: bool: Whether or not to start the line with a time stamp
@@ -49,20 +49,11 @@ class Logs(object):
 
         :return: None
         """
-        self.d_type = d_type
+        self.log_type = log_type
         self.directory = directory
         self.extension = extension
         self.write_time = write_time
         self.__check_exists()
-
-    def delete(self):
-        """
-        :return:
-        """
-        try:
-            os.remove(self.__get_path())
-        except FileNotFoundError:
-            pass
 
     def add(self, text: str, write_time=True) -> None:
         """
@@ -94,6 +85,70 @@ class Logs(object):
         with open(file_path, "a") as log:
             log.write("%s : %s\n" % (time_str, text))
 
+    def delete(self):
+        """
+        AUTHORS:
+        --------
+
+        :author: Alix Leroy and Samuel Westlake
+
+        DESCRIPTION:
+        ------------
+
+        Delete the log file.
+
+        PARAMETERS:
+        -----------
+
+        None
+
+        RETURN:
+        -------
+
+        :return: None
+
+        """
+        try:
+            os.remove(self.__get_path())
+        except FileNotFoundError:
+            pass
+
+    def close(self):
+        """
+        AUTHORS:
+        --------
+
+        :author: Alix Leroy and Samuel Westlake
+
+        DESCRIPTION:
+        ------------
+
+        Close the log file by renaming it to include a timestamp from the last line
+
+        PARAMETERS:
+        -----------
+
+        None
+
+        RETURN:
+        -------
+
+        :return: None
+
+        """
+        # We need a timestamp to give the log file a unique name.
+        # The timestamp from the last line of the log file is preferred over datetime.now() ...
+        # because we may be cleaning up and closing an old logfile from a previous, interrupted run.
+        with open(self.__get_path(), "r") as file:
+            lines = file.readlines()
+        try:
+            time = re.split("-| |:", lines[-1].split(".")[0])
+            time = tuple(map(int, time))
+            time = "%i-%i-%i_%i-%i-%i" % time
+        except (IndexError, ValueError):
+            time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        shutil.move(self.__get_path(), self.__get_path(time))
+
     def __check_exists(self) -> None:
         """
         AUTHORS:
@@ -119,28 +174,11 @@ class Logs(object):
             os.makedirs(self.directory, exist_ok=True)
             open(self.__get_path(), "w").close()
 
-    def close(self):
-        """
-        :return:
-        """
-        # We need a timestamp to give the log file a unique name.
-        # The timestamp from the last line of the log file is preferred over datetime.now() ...
-        # because we may be cleaning up and closing an old logfile from a previous, interrupted run.
-        with open(self.__get_path(), "r") as file:
-            lines = file.readlines()
-        try:
-            time = re.split("-| |:", lines[-1].split(".")[0])
-            time = tuple(map(int, time))
-            time = "%i-%i-%i_%i-%i-%i" % time
-        except (IndexError, ValueError):
-            time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        shutil.move(self.__get_path(), self.__get_path(time))
-
     def __get_path(self, time=None):
         """
         :return:
         """
         if time is None:
-            return "%s/%s%s" % (self.directory, self.d_type, self.extension)
+            return "%s/%s%s" % (self.directory, self.log_type, self.extension)
         else:
-            return "%s/%s_%s%s" % (self.directory, self.d_type, time, self.extension)
+            return "%s/%s_%s%s" % (self.directory, self.log_type, time, self.extension)
