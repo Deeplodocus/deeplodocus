875a19b8d978d7061f632b7c8220066b1a08fa00
Unknown <alix.m.leroy@cranfield.ac.uk>
2018-12-27 13:54:21 +0000

    Add entries with new flag system

--- deeplodocus/utils/flags/entry.py
+++ deeplodocus/utils/flags/entry.py
@@ -1,3 +1,5 @@
+from deeplodocus.utils import get_main_path
+from deeplodocus.utils.flag import Flag
 #
 # ENTRIES
 #
@@ -5,3 +7,11 @@ DEEP_ENTRY_INPUT = 0
 DEEP_ENTRY_LABEL = 1
 DEEP_ENTRY_OUTPUT = 2
 DEEP_ENTRY_ADDITIONAL_DATA = 3
+
+
+DEEP_ENTRY_INPUT_ = Flag(description="Input entry", names = ["input", "inputs", "inp", "x"])
+DEEP_ENTRY_LABEL_ = Flag(description="Label entry", names = ["label", "labels", "lab", "expected output", "expected_output", "y_expected", "y_hat"])
+DEEP_ENTRY_OUTPUT_ = Flag(description="Output entry", names=["output", "outputs", "y"])
+DEEP_ENTRY_ADDITIONAL_DATA_ = Flag(description="Additional Data entry", names=["additional_data", "additional data"])
+
+DEEP_ENTRY_BASE_FILE_NAME = get_main_path() + "data/auto-generated_entry_%s_%i.dat"
