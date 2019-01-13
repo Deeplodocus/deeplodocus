c1cfc339beec0dd7ea0f19a718dd69e891b1fdee
Unknown <alix.m.leroy@cranfield.ac.uk>
2019-01-13 15:07:21 +0000

    Update notification system with new Flag system

--- test/utils/notification.py
+++ test/utils/notification.py
@@ -3,14 +3,14 @@ Authors : Alix Leroy
 Test the different notification available
 """
 
-from deeplodocus.utils.flags import *
+from deeplodocus.utils.flags.notif import *
 from deeplodocus.utils.notification import Notification
 
-Notification(DEEP_NOTIF_WARNING, "Warning notification", write_logs=False)
-Notification(DEEP_NOTIF_INFO, "Info notification", write_logs=False)
-Notification(DEEP_NOTIF_SUCCESS, "Success notification", write_logs=False)
-Notification(DEEP_NOTIF_ERROR, "Error notification", write_logs=False)
-Notification(DEEP_NOTIF_DEBUG, "Debug notification", write_logs=False)
-Notification(DEEP_NOTIF_INPUT, "Input notification, please enter anything to proceed", write_logs=False)
-Notification("Not a good type", "Default notification", write_logs=False)
-Notification(DEEP_NOTIF_FATAL, "Fatal error exiting the program", write_logs=False)
+Notification(DEEP_NOTIF_WARNING, "Warning notification", log=False)
+Notification(DEEP_NOTIF_INFO, "Info notification", log=False)
+Notification(DEEP_NOTIF_SUCCESS, "Success notification", log=False)
+Notification(DEEP_NOTIF_ERROR, "Error notification", log=False)
+Notification(DEEP_NOTIF_DEBUG, "Debug notification", log=False)
+Notification(DEEP_NOTIF_INPUT, "Input notification, please enter anything to proceed", log=False)
+Notification(DEEP_NOTIF_LOVE, "Love notif when exiting the program", log=False)
+Notification(DEEP_NOTIF_FATAL, "Fatal error returning to the main loop", log=False)
