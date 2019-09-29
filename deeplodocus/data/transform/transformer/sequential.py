2f7d4dbd6ca4bb3917c777989cef2badafc9d4ef
samuelwestlake <samuelwestlake@hotmial.co.uk>
2019-09-29 17:22:43 +0100

    Fix configuration docs

--- deeplodocus/data/transform/transformer/sequential.py
+++ deeplodocus/data/transform/transformer/sequential.py
@@ -47,10 +47,12 @@ class Sequential(Transformer):
 
         :return: None
         """
-        super().__init__(name=name,
-                         mandatory_transforms_start=mandatory_transforms_start,
-                         transforms=transforms,
-                         mandatory_transforms_end=mandatory_transforms_end)
+        super().__init__(
+            name=name,
+            mandatory_transforms_start=mandatory_transforms_start,
+            transforms=transforms,
+            mandatory_transforms_end=mandatory_transforms_end
+        )
 
     def transform(self, transformed_data: Any, index: int, augment: bool) -> Any:
         """
